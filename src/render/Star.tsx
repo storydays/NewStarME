b66d9d8b0a74257dd8517978a1e66705245abfcb
Etienne M <em85@pm.me>
2025-06-28 05:41:17 +0200

    Updated Star.tsx

--- src/render/Star.tsx
+++ src/render/Star.tsx
@@ -255,7 +255,7 @@ function HighlightedStar({
           map={starTexture}
           transparent
           depthWrite={false}
-          opacity={1.0}
+          opacity={0.0} 
           color={ 'white'}
           alphaTest={0.1}
           blending={THREE.AdditiveBlending}
